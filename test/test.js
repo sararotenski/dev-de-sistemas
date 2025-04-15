d74f861098dfd4f702fca4a097c49bdeb3647918
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2025-04-15 10:19:39 -0300

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,24 @@
+let carro = "ford";
+
+switch(carro) {
+    case "ferrari":
+        console.log("O carro é uma Ferrari.");
+        break;
+    case "honda":
+        console.log("O carro é um Honda.");
+        break;
+    case "mercedes":
+        console.log("O carro é uma Mercedes.");
+        break;
+    case "ford":
+        console.log("O carro é um Ford.");
+        break;
+    case "BMW":
+        console.log("O carro é uma BMW.");
+        break;
+    case "mitsubichi":
+        console.log("O carro é uma Mitsubichi.");
+        break;
+        default:
+            console.log("Carro desconhecido.");
+}
